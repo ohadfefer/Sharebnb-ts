7dce61a81df8373c67dab2bc6a847fa15dc5b80a
Ohad <ohadfefer16@gmail.com>
2026-01-18 09:45:26 +0200

    Update package.json and package-lock.json to include TypeScript 5.9.3. Add TypeScript build and typecheck scripts. Modify tsconfig.json to enhance type checking and include TypeScript files in the project structure.

---
+++ back-sharebnb/dist/middlewares/logger.middleware.js
@@ -0,0 +1,6 @@
+import { logger } from '../services/logger.service.js';
+export async function log(req, res, next) {
+    const { baseUrl, method, body, params } = req;
+    logger.info(baseUrl, method, body, params);
+    next();
+}
